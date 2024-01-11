790cec9e45eb196b1620f4cfac087313711c944d
rickadobearup1 <156096875+rickadobearup1@users.noreply.github.com>
2024-01-11 14:53:44 +0000

    no interval

--- script.js
+++ script.js
@@ -414,19 +414,19 @@ document.addEventListener("DOMContentLoaded", function () {
       updateProfileInfo(userData);
     }
 
-    const intervalCallback = async () => {
-      // Fetch user data using the stored email
-      const userData = await fetchUserData(userEmail);
+    // const intervalCallback = async () => {
+    //   // Fetch user data using the stored email
+    //   const userData = await fetchUserData(userEmail);
     
-      // Update the profile information on the page
-      updateProfileInfo(userData);
-    };
+    //   // Update the profile information on the page
+    //   updateProfileInfo(userData);
+    // };
     
-    // Call the async function once immediately (before the interval starts)
-    intervalCallback();
+    // // Call the async function once immediately (before the interval starts)
+    // intervalCallback();
     
-    // Set up the setInterval to call the async function
-    setInterval(intervalCallback, 7 * 24 * 60 * 60 * 1000); // 7 days
+    // // Set up the setInterval to call the async function
+    // setInterval(intervalCallback, 7 * 24 * 60 * 60 * 1000); // 7 days
 
     const logoutButtons = document.querySelectorAll(".logout-btn");
 
